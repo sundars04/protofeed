a76ae342a08b841c9b47d6086b6875227b3f6baf
Sundars0 <Sundars04@outlook.com>
2017-01-03 07:55:10 +0000

    add authentication for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
+  before_action :logged_in_user, only:[:edit, :update, :destroy]
+  before_action :correct_user, only:[:edit, :update, :destroy]
 
   def show
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
   end
 
   def new
@@ -19,9 +21,35 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit    
+  end
+
+  def update    
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end    
+
+    # Confirms a logged in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end      
+    end
+
+    # Confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])      
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,11 +12,16 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])      
+    elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
