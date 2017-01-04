2946e3e65d71cd56f3b568ed2944b6f2047c43a0
Sundars0 <Sundars04@outlook.com>
2017-01-04 05:57:51 +0000

    add password_reset controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root 'static_pages#home'
   
   get '/help',      to: 'static_pages#help'
@@ -13,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
