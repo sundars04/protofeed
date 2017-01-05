df886fa9861efa2f0afea857c35c5c4060b1d3ff
Sundars0 <Sundars04@outlook.com>
2017-01-05 06:55:09 +0000

    modify carrier config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_provider = 'aws'
+  CarrierWave.configure do |config|    
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
